9e1693b5ba634a24a790e7c1329fbca64fe03a9f
Tulis <tulis@users.noreply.github.com>
2021-12-13 01:04:24 +1100

    Create edit-file-different-filesize.py

---
+++ io/edit-file-different-filesize.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Modify the file which may results into larger file or smaller file than the original file size, 
+# then we can't write to same file while we are reading it. But, says if we only change from 
+# upper case to lower case, then we can modify the file using the same file descriptor. But, this
+# following script is only catering for first case.
+
+import os
+
+offset = 0
+with open(file="./sample.env", mode='r') as file_input \
+    , open(file="./sample-output.env", mode='w', encoding='utf-8') as file_output:
+
+    for line in file_input:
+        print(line)
+        if line.startswith("moo="):
+            print("moo found")
+            start_indexOf = line.find("[") + 1
+            end_indexOf = line.find("]")
+            print("[ = ", start_indexOf, "; ] = ", end_indexOf)
+            isMatched = line.find("v10,", start_indexOf, end_indexOf) >= 0 \
+                or line.find("v10]", start_indexOf, end_indexOf + 1) >= 0
+
+            new_line = ""
+            if isMatched == False:
+                new_line = line[0:end_indexOf] + ",v10" + line[end_indexOf] + "\n"
+                print("new_line => ", new_line)
+
+                file_output.write(new_line)
+            else:
+                offset = offset + len(line)
+                file_output.write(line)
+        else:
+            offset = offset + len(line)
+            file_output.write(line)
+
+
+os.rename(src="./sample-output.env", dst="./sample-out.env")
