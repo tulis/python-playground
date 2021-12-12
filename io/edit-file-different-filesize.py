d2b1e4964d12b67f52aa1c08cfbf63db669933fa
Tulis <tulis@users.noreply.github.com>
2021-12-13 01:23:23 +1100

    Add enumerating argument variables that is supplied from command line

--- io/edit-file-different-filesize.py
+++ io/edit-file-different-filesize.py
@@ -5,7 +5,32 @@
 # upper case to lower case, then we can modify the file using the same file descriptor. But, this
 # following script is only catering for first case.
 
+# Sample how to run:
+# chmod 755 edit-file-different-filesize.py
+# edit-file-different-filesize.py v100
+
+# Sample output:
+# sys.argv[0] = ./edit.py
+# sys.argv[1] = v100
+# foo=abcdef
+#
+# bar=12343-1344378
+#
+# moo=[v1,v2,v3,v101,v100,v1000]
+#
+# moo found
+# [ =  5 ; ] =  29
+# new_line =>  moo=[v1,v2,v3,v101,v100,v1000,v10]
+#
+# zoo=https://www.twitter.com
+
 import os
+import sys
+
+keyword=""
+for index, argument in enumerate(sys.argv):
+    keyword=sys.argv[index]
+    print("sys.argv[%d] = %s" % (index, keyword))
 
 offset = 0
 with open(file="./sample.env", mode='r') as file_input \
